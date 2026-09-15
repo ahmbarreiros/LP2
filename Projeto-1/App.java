271ee6612c767115bad6a53f6ee2cc7e9fb05bf6
ahmbarreiros <ahmbarreiros.cs@gmail.com>
2026-09-15 16:52:50 -0300

    Questão 2 - Foco por Teclado

--- Projeto-1/App.java
+++ Projeto-1/App.java
@@ -43,7 +43,7 @@ class ListFrame extends JFrame {
                                       mousePosYPressed = evt.getY();
                                       
                                       
-
+                                      // Ao pressionar o mouse, itera sobre a lista de figuras e avalia as operações disponíveis sobre ela caso o mouse esteja sobre a figura
                                       focus = null;
                                       for (Figure fig: figs) {
                                           fig.oper(evt.getX(), evt.getY());
@@ -54,6 +54,7 @@ class ListFrame extends JFrame {
                                       repaint();
                                   }
                                   public void mouseReleased(MouseEvent evt) {
+                                      // Reavalia o marcador de ação de uma figura em foco ao parar de pressionar o botão do mouse
                                       if (focus != null) {
                                           focus.oper(evt.getX(), evt.getY());
                                           switch(focus.drag) {
@@ -96,6 +97,7 @@ class ListFrame extends JFrame {
 
         this.addMouseMotionListener(new MouseMotionAdapter() {
                 public void mouseDragged(MouseEvent evt) {
+                    // Ao arrastar o mouse, avalia o marcador de ação de uma figura em foco e realiza uma operação caso haja uma ação em curso.
                     mousePosX = evt.getX();
                     mousePosY = evt.getY();
                     if (focus != null) {
@@ -139,6 +141,7 @@ class ListFrame extends JFrame {
                 
 
                 public void mouseMoved(MouseEvent evt) {
+                    // Avalia o marcador de ação de uma figura em foco e altera o cursor caso a ação coincida com a posição do mouse
                     if (focus != null) {
                                           focus.oper(evt.getX(), evt.getY());
                                           switch(focus.drag) {
@@ -187,36 +190,49 @@ class ListFrame extends JFrame {
         this.addKeyListener (
             new KeyAdapter() {
                 public void keyPressed (KeyEvent evt) {
+
+                    // Cria uma figura "Retângulo", na posição central do mouse, com borda padrão 2 e marcador de ação vazio
                     if (evt.getKeyChar() == 'r' || evt.getKeyChar() == 'R') {
                         Rect rect = new Rect(mousePosX-25, mousePosY-25, 50, 50, 0, 2.0f, "");
                         figs.add(rect);
                         focus = rect;
                         repaint();  // outer.repaint()
                     }
+
+                    // Cria uma figura "Ellipse", na posição central do mouse, com borda padrão 2 e marcador de ação vazio
                     if (evt.getKeyChar() == 'e' || evt.getKeyChar() == 'E') {
                         Ellipse ellipse = new Ellipse(mousePosX-40, mousePosY-25, 80, 50, 0, 2.0f, "");
                         figs.add(ellipse);
                         focus = ellipse;
                         repaint();  // outer.repaint()
                     }
+
+                    // Cria uma figura "Arco", na posição central do mouse, com borda padrão 2 e marcador de ação vazio
                     if (evt.getKeyChar() == 'a' || evt.getKeyChar() == 'A') {
                         Arc arc = new Arc(mousePosX-25, mousePosY-25, 50, 50, 0, 2.0f, "");
                         figs.add(arc);
                         focus = arc;
                         repaint();  // outer.repaint()
                     }
+
+                    // Cria uma figura "Reta", na posição central do mouse, com borda padrão 2 e marcador de ação vazio
                     if (evt.getKeyChar() == 'l' || evt.getKeyChar() == 'L') {
                         Line line = new Line(mousePosX-25, mousePosY-25, 50, 50, 0, 2.0f, "");
                         figs.add(line);
                         focus = line;
                         repaint();  // outer.repaint()
                     }
-		    if (evt.getKeyChar() == '1') {
+
+                    // Cria uma figura "Carro", na posição central do mouse, com borda padrão 2 e marcador de ação vazio
+		            if (evt.getKeyChar() == '1') {
                         Carro carro = new Carro(mousePosX-25, mousePosY-25, 50, 50, 0, 2.0f, "");
+                        // Adiciona o carro da lista de figuras e deixa o carro em foco
                         figs.add(carro);
                         focus = carro;
                         repaint();  // outer.repaint()
                     }
+
+                    // Deleta a figura em foco
                     if (evt.getKeyCode() == KeyEvent.VK_DELETE) {
                         if (focus != null) {
                             figs.remove(focus);
@@ -224,39 +240,63 @@ class ListFrame extends JFrame {
                         	repaint();
                         }
                     }
+
+                    // Altera para a próxima cor de contorno
                     if (evt.getKeyCode() == KeyEvent.VK_RIGHT) {
                         if (focus != null) {
                             focus.changeBorderR();
                             repaint();
                         }
                     }
+
+                    // Altera para a cor de contorno anterior
                     if (evt.getKeyCode() == KeyEvent.VK_LEFT) {
                         if (focus != null) {
                             focus.changeBorderL();
                             repaint();
                         }
                     }
+
+                    // Altera para a próxima cor de fundo
                     if (evt.getKeyCode() == KeyEvent.VK_UP) {
                         if (focus != null) {
                             focus.changeFillU();
                             repaint();
                         }
                     }
+
+                    // Altera para a cor de fundo anterior
                     if (evt.getKeyCode() == KeyEvent.VK_DOWN) {
                         if (focus != null) {
                             focus.changeFillD();
                             repaint();
                         }
                     }
+
+                    // Aumenta a largura do contorno
                     if (evt.getKeyChar() == '=' || evt.getKeyChar() == '+') {
                         if (focus != null) {
                             focus.changeBorderU();
                             repaint();
                         }
-                    }	
+                    }
+
+                    // Diminui a largura do contorno
                     if (evt.getKeyChar() == '-') {
                         if (focus != null) {
                             focus.changeBorderD();
+                            repaint();
+                        }
+                    }
+                    
+                    // Atalho para alterar foco da figura
+                    if (evt.getKeyChar() == '2') {
+                        // Só altera o foco caso haja alguma figura já em foco
+                        if (focus != null) {
+                            // Guarda o indice da figura em foco na lista de figuras
+                            int i = figs.indexOf(focus);
+                            // Passa o foco para a próxima figura da lista, e caso esteja no fim, volta para o início da lista
+                            focus = figs.get((i+1) % figs.size());
                             repaint();
                         }
                     }
@@ -272,10 +312,12 @@ class ListFrame extends JFrame {
         super.paint(g);
         Graphics2D g2d = (Graphics2D) g;
 
+        // Para cara figura da lista, pinta ela com o seu próprio método
         for (Figure fig: this.figs) {
             fig.paint(g);
         }
 
+        // Caso haja uma figura em foco, pinta o retângulo que a destaca, de acordo com o tipo de figura
         if (focus != null) {
             focus.paintFocus(g, 3, 3);
         }
